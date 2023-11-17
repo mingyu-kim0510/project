7930e30b35e52048cf169912860cf3a71b4134cc
parkjonghyukp <jonghyukpark001@gmail.com>
2023-11-17 09:19:43 +0900

    feat: 누락파일 추가

---
+++ src/main/java/com/example/map_test/dto/DistrictResDto.java
@@ -0,0 +1,16 @@
+package com.example.map_test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DistrictResDto {
+    private String districtName;
+    private String predictCongestion;
+    private int storeCount;
+}
