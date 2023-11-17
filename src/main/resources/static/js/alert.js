8e671e8eae6f07cb75384b5af9ae7dd7221a97b6
parkjonghyukp <jonghyukpark001@gmail.com>
2023-11-17 15:46:36 +0900

    feat: 누락파일 추가

---
+++ src/main/resources/static/js/alert.js
@@ -0,0 +1,12 @@
+function alertModal(modalContent) {
+    return `
+        <!-- Alert Modal -->
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body" style="background-color:#f8d7da !important; color: #58151c; border:1px solid #f1aeb5; border-radius: 0.375rem;">
+                    ${modalContent}
+                </div>
+            </div>
+        </div>
+    `
+}
